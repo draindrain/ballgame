0cc3a332b0152d79f9892d9a55577c7dc75533a8
Johan Drake <johdra@gmail.com>
2025-01-13 20:10:35 +0100

    Added a basic scene

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,7 +1,33 @@
+import { Environment, OrbitControls, Plane, Sphere } from "@react-three/drei"
 import "./Game.css"
+import EnvMap from "./EnvMap"
 
 function Game() {
-    return <></>
+    return (
+        <>
+            <OrbitControls />
+
+            {/* Lighting */}
+            <Environment
+                background
+                files='./static/kloofendal_48d_partly_cloudy_puresky_1k.hdr'
+            />
+
+            {/* Plane */}
+            <Plane
+                args={[10, 10]} // Width and height of the plane
+                rotation={[-Math.PI / 2, 0, 0]} // Rotate the plane to be horizontal
+                position={[0, -0.5, 0]} // Position it slightly below the sphere
+            >
+                <meshStandardMaterial color='lightgrey' />
+            </Plane>
+
+            {/* Sphere (Ball) */}
+            <Sphere args={[0.5, 32, 32]} position={[0, 0.5, 0]}>
+                <meshStandardMaterial color='blue' />
+            </Sphere>
+        </>
+    )
 }
 
 export default Game
